1668f0d970dfc61eeb7248b0579fe9345bb453bf
Kosuke Arisawa <arisawa@gmail.com>
2022-09-21 10:34:39 +0900

    Impelment module ActionController::Twirp

--- lib/action_controller/twirp.rb
+++ lib/action_controller/twirp.rb
@@ -4,7 +4,26 @@ require 'action_controller/twirp/version'
 require 'action_controller/twirp/railtie'
 
 module ActionController
+  # Module to execute your implemented methods on Twirp with Rails
   module Twirp
-    # Your code goes here...
+    private
+
+    # Override it to call a twirp class from request path
+    def send_action(*_args)
+      twirp_service_class.raise_exceptions = true
+
+      status, header, body = twirp_service_class.new(self)&.call(request.env)
+
+      response.status = status
+      response.header.merge(header)
+      response.body = body
+    end
+
+    def twirp_service_class
+      return @twirp_service_class if defined? @twirp_service_class
+
+      class_name = request.path.split('/')[-2].underscore.gsub('.', '/').classify
+      @twirp_service_class = "#{class_name}Service".constantize
+    end
   end
 end
