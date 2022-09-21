d9ba4c6e30e6303b629cdb5c06a29943bc50509d
Kosuke Arisawa <arisawa@gmail.com>
2022-09-21 10:34:21 +0900

    Add #render option :twirp_error

--- lib/action_controller/twirp/railtie.rb
+++ lib/action_controller/twirp/railtie.rb
@@ -5,7 +5,15 @@ require 'action_dispatch/twirp/railtie'
 
 module ActionController
   module Twirp
-    class Railtie < ::Rails::Railtie
+    class Railtie < ::Rails::Railtie # :nodoc:
+      initializer 'action_controller.twirp' do
+        ActiveSupport.on_load(:action_controller) do
+          ActionController::Renderers.add :twirp_error do |content, options|
+            renderer = TwirpErrorRenderer.new(content, options)
+            render(**renderer.to_render_option)
+          end
+        end
+      end
     end
   end
 end
